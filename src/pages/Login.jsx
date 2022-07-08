c44434b81d649ae58e3dffdce0a3bb8f5eb36836
Everth Paredes <epe0413@gmail.com>
2022-07-08 11:49:31 -0600

    Se enlazaron paginas de registrar y olvide-password a la pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
-
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <>
         <h1 className="text-sky-600 font-black text-4xl md:text-5xl capitalize text-center">Inicia sesión y administra tus <span className="text-slate-700">proyectos</span> </h1>
 
-        <form className="my-10 bg-white shadow rounded-lg p-5">
+        <form className="my-10 mx-5 bg-white shadow rounded-lg p-5">
             <div className="my-5">
                 <label
                     className="uppercase text-gray-700 block text-xl font-bold"
@@ -38,6 +38,21 @@ const Login = () => {
                 className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5"
             />
         </form>
+
+        <nav className="lg:flex lg:justify-between">
+            <Link
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/registrar"
+            >
+            ¿No tienes una cuenta? Regístrate
+            </Link>
+            <Link
+                className='block text-center my-5 text-slate-500 uppercase text-sm'
+                to="/olvide-password"
+            >
+            Olvide mi password
+            </Link>
+        </nav>
     </>
   )
 }
